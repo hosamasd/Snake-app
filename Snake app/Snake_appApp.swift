37547f429bdad533a213c2f88bff63343afc5d56
hosam <hosamelmalt@gmail.com>
2021-09-12 14:18:35 +0200

    Initial Commit

---
+++ Snake app/Snake_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Snake_appApp.swift
+//  Snake app
+//
+//  Created by hosam on 12/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct Snake_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
